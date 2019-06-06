4ed86adfff8ef5e59095e3b9bfe3391a3ec1eae8
stephenwf <stephen.fraser@digirati.com>
2019-06-06 14:34:36 +0100

    Fixed smoke test paths

--- .travis/test-script.sh
+++ .travis/test-script.sh
@@ -5,4 +5,4 @@ set -ex
 [[ "$(curl -sS "http://localhost:8898/" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
 
 # Test example site
-[[ "$(curl -sS "http://localhost:8898/s/default/page/welcome" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
+[[ "$(curl -sS "http://localhost:8898/s/default/page/homepage" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
